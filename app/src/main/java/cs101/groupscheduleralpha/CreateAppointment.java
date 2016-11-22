d37a74f0727bc7ed573dbb59aa313035bf8240f7
Abhilaash Velamati <abhilaash.velamati@gmail.com>
2016-11-21 20:12:14 -0500

    CreateAppointment Intent Created

--- app/src/main/java/cs101/groupscheduleralpha/CreateAppointment.java
+++ app/src/main/java/cs101/groupscheduleralpha/CreateAppointment.java
@@ -1,18 +1,31 @@
 package cs101.groupscheduleralpha;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAppointment extends AppCompatActivity{
-
+Button bSetApp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_app);
         setTitle("Create an Appointment");
+        bSetApp = (Button) findViewById(R.id.button3);
+        bSetApp.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View view){
+                        Intent intent = new Intent(getApplicationContext(), Welcome.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
     }
 
-    public void sendToCloud(){
-        setContentView(R.layout.welcome);
-    }
+//    public void sendToCloud(){
+//
+//    }
 }
