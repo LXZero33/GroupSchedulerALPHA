6c5a57504eef174b75a1ee12ac0b5c0cb269624b
Abhilaash Velamati <abhilaash.velamati@gmail.com>
2016-11-21 22:35:22 -0500

    I think I got all of the intents

--- app/src/main/java/cs101/groupscheduleralpha/Welcome.java
+++ app/src/main/java/cs101/groupscheduleralpha/Welcome.java
@@ -1,18 +1,29 @@
 package cs101.groupscheduleralpha;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class Welcome extends AppCompatActivity {
-
+Button setApp;
+Button checkApp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
         setTitle("Group Scheduler");
-
+        setApp = (Button) findViewById(R.id.createapp);
+        bSetApp.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View view){
+                        Intent intent = new Intent(getApplicationContext(), Welcome.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 
     public void setAppointment(){
